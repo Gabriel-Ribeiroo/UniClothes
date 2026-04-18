04e3f68191dc3a0bb8ed12a4d0617e909686470b
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2026-04-18 12:03:07 -0300

    feat: add mobile version of sidebar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// THEME 
 const themeToggler = document.querySelector("[data-js='theme-toggler']")
 
 themeToggler.addEventListener("click", () => {
@@ -10,4 +11,26 @@ themeToggler.addEventListener("click", () => {
 
 	root.classList.remove("light")
 	root.classList.add("dark")
+})
+
+// SIDEBAR 
+const sidebar = document.querySelector("[data-js='sidebar']")
+
+const sidebarOpen = document.querySelector("[data-js='sidebar-open']")
+
+sidebarOpen.addEventListener("click", () => {
+
+	if (sidebar.classList.contains("hidden")) {
+		sidebar.classList.remove("hidden")
+		sidebar.classList.add("visible")
+	}
+})
+
+const sidebarClose = document.querySelectorAll("[data-js='sidebar-close']")
+
+sidebarClose.forEach(close => {
+	close.addEventListener("click", () => {
+		sidebar.classList.remove("visible")
+		sidebar.classList.add("hidden")
+	})
 })

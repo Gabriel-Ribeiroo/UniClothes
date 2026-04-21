c2d562a48fe1aa0a0c0114033fb39f521df7b20e
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2026-04-21 16:41:12 -0300

    refactor: remove unnecessary if condition from open sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,12 +1,9 @@
 const sidebar = document.querySelector("[data-js='sidebar']")
 
 export function openSidebar() {
-	if (sidebar.classList.contains("hidden")) {
-		sidebar.classList.remove("hidden")
-	}
+	sidebar.classList.remove("hidden")
 }
 
 export function closeSidebar() {
-	sidebar.classList.remove("visible")
 	sidebar.classList.add("hidden")
 }
